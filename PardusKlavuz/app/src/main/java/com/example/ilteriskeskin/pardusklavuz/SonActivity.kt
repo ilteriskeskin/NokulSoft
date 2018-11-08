0ef607733c7d9486d1ce82e511c32da874bb49da
Ali İlteriş Keskin <ilteriskesk20@gmail.com>
2018-11-09 02:09:35 +0300

    manifest dosyası güncellendi

--- PardusKlavuz/app/src/main/java/com/example/ilteriskeskin/pardusklavuz/SonActivity.kt
+++ PardusKlavuz/app/src/main/java/com/example/ilteriskeskin/pardusklavuz/SonActivity.kt
@@ -1,7 +1,10 @@
 package com.example.ilteriskeskin.pardusklavuz
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
@@ -19,5 +22,10 @@ class SonActivity : AppCompatActivity() {
         mAdView = findViewById(R.id.adView)
         val adRequest = AdRequest.Builder().build()
         mAdView.loadAd(adRequest)
+    }
+
+    fun site(view: View) {
+        val i = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.nokulsoft.com"))
+        startActivity(i)
     }
 }
